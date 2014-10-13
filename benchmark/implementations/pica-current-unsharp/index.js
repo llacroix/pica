3fe3e04f4bcd611b8da4fc93a51e430838a8e821
Loïc Faure-Lacroix <lamerstar@gmail.com>
2014-10-13 19:32:26 +0400

    Fix benchmark unsharp implementation: Prevent copying big buffer
    The sample buffer has size of 32mb. Copying the buffer takes a lot
    of time. The filter will modify the buffer in place instead of copying
    it multiple times.

--- benchmark/implementations/pica-current-unsharp/index.js
+++ benchmark/implementations/pica-current-unsharp/index.js
@@ -3,10 +3,8 @@
 var unsharp = require('../../../lib/pure/unsharp');
 
 exports.run = function(data) {
-  var b;
-  b = new Uint8Array(data.buffer.length);
-  b.set(data.buffer);
-
-  unsharp(b, data.width, data.height, 100, 1.0, 0);
-  return b;
+  // We reuse the same buffer even if the filter
+  // changes it. Allocating the sample buffer takes too
+  // much time
+  return unsharp(data.buffer, data.width, data.height, 100, 1.0, 0);
 };
